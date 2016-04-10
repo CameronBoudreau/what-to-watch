1f29e41650f6d308add5f25331389681e7a361b1
CameronB <cboudreau6238@gmail.com>
2016-04-09 23:39:37 -0400

    Passes first round tests under 1 sec - down from 16.

--- watch_tests.py
+++ watch_tests.py
@@ -1,18 +1,26 @@
 from what_to_watch import *
 
+movie_ratings_dict = get_movie_ratings_dict()
+movie_dict = get_movie_dict(movie_ratings_dict)
+id_to_title = set_ids_to_titles(movie_dict)
+user_ratings_dict = get_user_ratings_dict()
+user_dict = get_user_dict(user_ratings_dict)
 
 
 def test_get_movie_ratings():
-    pass
-
+    assert movie_ratings_dict[16][:3] == [(10, 4), (181, 1), (268, 3)]
 
 def test_average_ratings():
-    pass
+    assert (movie_dict[44].average) == 3.34
 
 
 def test_find_movie_by_id():
-    pass
+    assert id_to_title[2] == 'GoldenEye (1995)'
 
 
-def test_find_ratings_from_user():
-    pass
+def test_get_user_ratings():
+    assert user_ratings_dict[2][:3] == [(292, 4), (251, 5), (50, 5)]
+
+
+def test_find_ratings_in_user_object():
+    assert user_dict[356].ratings[:3] == [(313, 5), (937, 2), (689, 5)]
--- what_to_watch.py
+++ what_to_watch.py
@@ -10,10 +10,11 @@ def clear():
 
 
 class Movie:
-    def __init__(self, row):
+    def __init__(self, row, ratings):
         self.id = int(row['MovieID'])
         self.title = row['MovieTitle']
-        self.average_rating = None
+        self.ratings = ratings
+        self.average = self.find_average_rating(ratings)
 
     def __str__(self):
         return "{}: {}".format(self.id, self.title)
@@ -21,17 +22,23 @@ class Movie:
     def __repr__(self):
         return str(self)
 
+    def find_average_rating(self, ratings):
+        total = sum([t[1] for t in ratings])
+        return float("%.2f" % (total / len(ratings)))
+
+
 
 
 class User:
-    def __init__(self, row):
+    def __init__(self, row, ratings):
         self.user_id = int(row['UserID'])
         self.age = int(row['Age'])
         self.sex = row['Sex']
         self.occupation = row['Occupation']
+        self.ratings = ratings
 
     def __str__(self):
-        return str("{}: Age: {}; Sex: {}; Occupation: {}; Zipcode: {}".format(self.user_id, self.age, self.sex, self.occupation, self.zipcode))
+        return str("{}: Age: {}; Sex: {}; Occupation: {}; Ratings: {}".format(self.user_id, self.age, self.sex, self.occupation, self.ratings))
 
     def __repr__(self):
         return str(self)
@@ -39,7 +46,7 @@ class User:
 
 
 class Rating:
-    def __init__(self, row):
+    def __init__(self, row, ratings):
         self.user_id = int(row['UserID'])
         self.movie_id = int(row['MovieID'])
         self.rating = int(row['Rating'])
@@ -52,45 +59,88 @@ class Rating:
 
 
 
-def get_movie_list():
+def get_movie_dict(movie_ratings_dict):
     with open('u.item', encoding='latin_1') as f:
-        movie_list = []
+        movie_dict = {}
         reader = csv.DictReader(f, fieldnames=['MovieID', 'MovieTitle'], delimiter='|')
         for row in reader:
-            movie_list.append(Movie(row))
-    return movie_list
+            movie_dict[int(row['MovieID'])] = Movie(row, movie_ratings_dict[int(row['MovieID'])])
+    return movie_dict
 
 
-def get_movie_ratings_list():
+def get_movie_ratings_dict():
     with open('u.data', encoding='latin_1') as f:
-        ratings_list = []
+        movie_ratings_dict = {}
         reader = csv.DictReader(f, fieldnames=['UserID', 'MovieID', 'Rating', 'Time'], delimiter='\t')
-        [ratings_list.append(Rating(row)) for row in reader]
-    return ratings_list
+        for row in reader:
+            if int(row['MovieID']) in movie_ratings_dict:
+                movie_ratings_dict[int(row['MovieID'])].append((int(row['UserID']), int(row['Rating'])))
+            else:
+                movie_ratings_dict[int(row['MovieID'])] = [(int(row['UserID']), int(row['Rating']))]
+    return movie_ratings_dict
 
 
-def get_user_list():
+def get_user_ratings_dict():
+    with open('u.data', encoding='latin_1') as f:
+        user_ratings_dict = {}
+        reader = csv.DictReader(f, fieldnames=['UserID', 'MovieID', 'Rating', 'Time'], delimiter='\t')
+        for row in reader:
+            if int(row['UserID']) in user_ratings_dict:
+                user_ratings_dict[int(row['UserID'])].append((int(row['MovieID']), int(row['Rating'])))
+            else:
+                user_ratings_dict[int(row['UserID'])] = [(int(row['MovieID']), int(row['Rating']))]
+    return user_ratings_dict
+
+
+def get_user_dict(user_ratings_dict):
         with open('u.user', encoding='latin_1') as f:
-            user_list = []
+            user_dict = {}
             reader = csv.DictReader(f, fieldnames=['UserID', 'Age', 'Sex', 'Occupation', "ZipCode"], delimiter='|')
-            [user_list.append(User(row)) for row in reader]
-        return user_list
+            for row in reader:
+                user_dict[int(row['UserID'])] = User(row, user_ratings_dict[int(row['UserID'])])
+        return user_dict
 
 
-def set_ids_to_titles(movie_list):
+def get_rating_list(movie_ratings_dict, row):
+    rating_list = []
+    for key in movie_ratings_dict:
+        for rating in movie_ratings_dict[key]:
+            if rating[0] == row['UserID']:
+                rating_list.append(rating)
+    return rating_list
+
+
+
+def set_ids_to_titles(movie_dict):
     id_to_title = {}
-    for m in movie_list:
-        id_to_title[m.id] = m.title
+    for i in movie_dict:
+        id_to_title[i] = movie_dict[i].title
     return id_to_title
 
 
 
 
 def main():
-    movie_list = get_movie_list()
-    id_to_title = set_ids_to_titles(movie_list)
-    print(id_to_title)
+    clear()
+    # ratings_dict = {MovieID: [user_id, rating]}
+    movie_ratings_dict = get_movie_ratings_dict()
+    # print(movie_ratings_dict[16][:3])
 
+    # movie_dict = {MovieID: MovieObject}
+    movie_dict = get_movie_dict(movie_ratings_dict)
+    # print(movie_dict[44].average)
+
+    # id_to_title = {ID: Title}
+    id_to_title = set_ids_to_titles(movie_dict)
+    # print(id_to_title[2])
+
+    # user_ratings_dict = {UserID:[movie_id, rating]}
+    user_ratings_dict = get_user_ratings_dict()
+    # print(user_ratings_dict[2][:3])
+
+
+    user_dict = get_user_dict(user_ratings_dict)
+    print(user_dict[356].ratings[:3])
 
 
 if __name__ == '__main__':
